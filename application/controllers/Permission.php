90a03dc112fe828074f7b7bb6a9696c09e9d5a23
zhengzean <zhengzean01@gmail.com>
2017-12-15 15:51:35 +0800

    group manage update

---
+++ application/controllers/Permission.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Permission.php
+ *
+ * 作者: zhengzean (zhengzean01@gmail.com)
+ * 创建日期: 2017/12/15 15:41
+ * 修改记录:
+ *
+ * $Id$
+ */
+
+class PermissionController extends ControllerAbstract
+{
+    public function indexAction()
+    {
+
+    }
+}
